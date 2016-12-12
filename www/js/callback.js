9c45a10b39244e587782e0adb31d245ece0c51a8
Max Klenk <max@klenk.biz>
2016-12-12 18:02:14 +0100

    merge server communication

--- www/js/callback.js
+++ www/js/callback.js
@@ -6,6 +6,27 @@ var CALLBACK_TYPES = {
 var DEFAULT_HEADERS = {
   'Content-Type': 'application/json'
 };
+// var SERVER_URL = 'https://schul-cloud.org:3030';
+var SERVER_URL = 'http://localhost:3030';
+
+function sendRegistrationId(id) {
+  var url = SERVER_URL + '/devices';
+  var body = {
+    "service": "firebase",
+    "type": "mobile",
+    "name": "test2",
+    "user_token": "usertoken1",
+    "service_token": id,
+    "OS": "android7"
+  };
+  var data = JSON.stringify(body);
+
+  function callback(response) {
+    console.log(response);
+  }
+
+  postRequest(url, data, callback);
+}
 
 function sendShownCallback(notificationData) {
   var body = {
@@ -34,21 +55,37 @@ function sendClickedCallback(notificationData) {
 }
 
 function sendCallback(body, callback) {
-  var url = 'http://localhost:3030/callback';
+  var url = SERVER_URL + '/callback';
   var data = JSON.stringify(body);
 
   postRequest(url, data, callback);
 }
 
 function postRequest(url, data, callback) {
-  var xhttp;
-  xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 201) {
-      callback(this);
-    }
-  };
-  xhttp.open('POST', url, true);
-  xhttp.setRequestHeader("Content-type", DEFAULT_HEADERS['Content-Type']);
-  xhttp.send(data);
+  if (self.fetch) {
+    fetch(url, {
+      method: 'POST',
+      body: data,
+      headers: DEFAULT_HEADERS
+    })
+      .then(function(response) {
+        response.json().then(function(json) {
+          callback(json);
+        });
+      });
+  } else if (self.XMLHttpRequest) {
+    var xhttp;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 201) {
+        var response = JSON.parse(xhttp.responseText);
+        callback(response);
+      }
+    };
+    xhttp.open('POST', url, true);
+    xhttp.setRequestHeader("Content-type", DEFAULT_HEADERS['Content-Type']);
+    xhttp.send(data);
+  } else {
+    console.log('No way to send out', data);
+  }
 }
--- www/js/index.js
+++ www/js/index.js
@@ -4,7 +4,7 @@ var pushManager = {
   setRegistrationId: function(id) {
     console.log('set registration id: ' + id);
 
-    schulcloud.sendRegistrationId(id);
+    sendRegistrationId(id);
   },
 
   error: function(error, msg) {
@@ -25,46 +25,4 @@ var pushManager = {
     console.log('server ', service, ' is set up!');
     this.requestPermissionCallback = requestPermissionCallback;
   }
-};
-
-var schulcloud = {
-
-  sendRegistrationId: function(id) {
-    var url = 'http://localhost:3030/devices';
-    var method = 'POST';
-    var body = {
-      "service": "firebase",
-      "type": "mobile",
-      "name": "test2",
-      "user_token": "usertoken1",
-      "service_token": id,
-      "OS": "android7"
-    };
-    var data = JSON.stringify(body);
-
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4 && xhr.status == 201) {
-        console.log(JSON.parse(xhr.responseText));
-      }
-    };
-    xhr.open(method, url, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(data);
-
-    // Alternative implementation with fetch()
-    // fetch(url, {
-    //   method: method,
-    //   body: data,
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   }
-    // })
-    //   .then(function(response) {
-    //     response.json().then(function(json) {
-    //       console.log(json);
-    //     })
-    //   });
-  }
-
 };
