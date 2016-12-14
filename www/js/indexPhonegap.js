e53d6ef70d882a16279243f8270ea6c41cdc8671
Max Klenk <max@klenk.biz>
2016-12-14 15:42:21 +0100

    add additional handling for iOS

--- www/js/indexPhonegap.js
+++ www/js/indexPhonegap.js
@@ -31,23 +31,46 @@ var setupPhonegapPush = function() {
   push.on('notification', function(data) {
     pushManager.handleNotification(data);
 
-    sendShownCallback(data.additionalData);
-
-    if (data.additionalData.news && !data.additionalData.foreground) {
-      // https://github.com/katzer/cordova-plugin-local-notifications/wiki/04.-Scheduling
-      cordova.plugins.notification.local.schedule({
-        title: data.additionalData.news.title,
-        text: data.additionalData.news.body,
-        data: data.additionalData
-      });
+    // This function is not called different on Android and iOS.
+    var isIOS = false;
+    if (data.additionalData && data.additionalData['gcm.message_id']) {
+      // the message was send using firebase(gcm) to APNs
+      isIOS = true;
     }
 
-    if (push.finish) {
-      push.finish(function() {
-        console.log('success');
-      }, function() {
-        console.log('error');
-      }, 'push-1');
+    if (isIOS) {
+
+      if (data.additionalData.foreground) {
+        // on iOS this function is called while the app was open
+        console.log('notification received', data);
+        sendShownCallback(data.additionalData);
+      } else {
+        // on iOS this function is called as click callback
+        console.log('notification clicked', data);
+        sendClickedCallback(data.additionalData);
+      }
+
+      if (push.finish) {
+        push.finish(function() {
+          console.log('success');
+        }, function() {
+          console.log('error');
+        }, 'push-1');
+      }
+
+    } else {
+
+      sendShownCallback(data.additionalData);
+
+      if (data.additionalData.news && !data.additionalData.foreground) {
+        // https://github.com/katzer/cordova-plugin-local-notifications/wiki/04.-Scheduling
+        cordova.plugins.notification.local.schedule({
+          title: data.additionalData.news.title,
+          text: data.additionalData.news.body,
+          data: data.additionalData
+        });
+      }
+
     }
   });
 
